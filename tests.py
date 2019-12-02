9940165d17e88f95c55ffd2e169fc7cb9943617e
edward <edward@e-ofisi.com>
2019-12-02 11:36:12 +0300

    first commit

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+from mymathlib import is_prime
+
+# print(is_prime(0))
+
+class IsPrimeTests(unittest.TestCase):
+    def test_zero(self):
+        self.assertFalse(is_prime(0))
+
+    def test_four(self):
+        self.assertFalse(is_prime(4))
+
+if __name__ == "__main__":
+    unittest.main()
